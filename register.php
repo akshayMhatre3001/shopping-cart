4a018d14bfca7f094ddede24ba47d85d144a2895
beastf589 <akshaymhatre89@yahoo.in>
2017-08-07 01:19:41 +0530

    register php

---
+++ register.php
@@ -0,0 +1,134 @@
+<html>
+<head>
+		<title>AIT Project</title>
+
+		
+		
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="main.css" />
+		
+		
+</head>
+<body>
+
+
+
+<div id="wrapper">
+
+				<div id="header">
+				
+				
+				
+				<div>
+				<!-- Logo -->
+<img src="logo.png" id="logos" alt="BVCVOE" >
+
+
+				
+				
+				<a href="registration.html">					
+ <input  id="sub" type="submit" value="Registration"  tabindex="4"/>
+</a>
+<a href="login.html">
+ <input  id="sub" type="submit" value="Login" tabindex="4"/>
+ </a>	
+	<h1><a href="index.html" id="logo"><center>AIT Project<center></a></h1>
+	
+					</div>
+					
+					<div class="topnav">
+					<!-- Nav -->
+					
+						<nav id="nav">
+							<ul>
+								<a href="index.html">Home   </a>
+								<a href="feedback.html">Feedback</a>
+								<a href="aboutus.html">Aboutus</a>
+								
+								<a href="contact.html">Contact</a>
+							</ul>
+						</nav>
+                                 
+					</div>	name
+                <div id="content">
+				</div>
+				<div id="left-panel">
+				</div>
+               <div id="right-panel">
+			   <form action="reg.php" method="post">
+			   <table border='0' width='480px' cellpadding='0' cellspacing='0' align='center'>
+<center><tr>
+   <td><h1><center>Registration Form</center></h1></td>
+</tr><center>
+<p></p>
+<p><table border='2' width='500px' cellpadding='5' cellspacing='1' align='center'></p>
+
+<tr>
+    <td align='center'>Name:</td>
+    <td><input type='text' name='' required></td>
+</tr>
+<tr> <td>&nbsp;</td> </tr>
+<tr>
+    <td align='center'>Sur Name:</td>
+    <td><input type='text' name='name' required></td>
+</tr>
+<tr> <td>&nbsp;</td> </tr>
+<tr>
+    <td align='center'>Date Of Birth:</td>
+    <td><input type='date' name='datename' required ></td>
+</tr>
+<tr> <td>&nbsp;</td> </tr>
+<tr>
+    <td align='center'>Address:</td>
+    <td><input type='text' name='addr' required></td>
+</tr>
+<tr> <td>&nbsp;</td> </tr>
+<tr>
+    <td align='center'>Phone:</td>
+    <td><input type='tel' name='telname' pattern='[789][0-9]{9}' required ></td>
+</tr>
+<tr> <td>&nbsp;</td> </tr>
+<tr>
+    <td align='center'>Email:</td>
+    <td><input type='email' name='email' autocomplete='off' required ></td>
+</tr>
+<tr> <td>&nbsp;</td> </tr>
+<tr>
+<tr>
+    <td align='center'>Password</td>
+    <td><input type='password' name='passname' autocomplete='off'required ></td>
+</tr>
+<tr> <td>&nbsp;</td> </tr>
+<table border='2' cellpadding='5' cellspacing='1' width='500px' align='center'>
+<tr>
+    <td align='center'><button type="submit" value="Submit">Submit</button>
+
+</td>
+    <td ><button type="reset" value="Reset">Reset</button></td>
+	
+	</tr>
+
+   </form>
+
+</table>
+</table>
+
+</table>
+
+
+
+
+				</div>
+			   
+			   <div id="footer">
+				</div>
+
+		
+
+</div>
+
+</body>
+</html>
